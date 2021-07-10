9e67cda83ad39862f264bc0e8dfdb271d6e9d65c
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-07-10 16:33:46 +0200

    Minor modification

--- RandomColor/ColorsDetailVC.swift
+++ RandomColor/ColorsDetailVC.swift
@@ -13,6 +13,6 @@ class ColorsDetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = color ?? .blue
+        view.backgroundColor = color ?? .white
     }
 }
