8511ca3e1c1968175424faabb357987a7446baf0
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-07-10 14:12:23 +0200

    Initial Commit

---
+++ RandomColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColor
+//
+//  Created by Enrico Catanzani on 10/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
